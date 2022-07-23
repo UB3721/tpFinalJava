6b5598f21584d176acfc94b90031d2aed688b2de
UB3721 <aspiranteypatito@gmail.com>
2022-07-23 15:53:52 -0400

    Muchos cambios en las clases, que incluyen pero no se limitan a: -Modificaciones de los atributos para su correcto almacenamiento -Cambio en los constructores (Principalmente,adaptacion a los modificados atributos -Introduccion de las formas de almacenamiento

--- tpFinalJava/src/main/java/com/uaa/modelo/Habitacion.java
+++ tpFinalJava/src/main/java/com/uaa/modelo/Habitacion.java
@@ -4,28 +4,133 @@
  */
 package com.uaa.modelo;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Gabriel
  */
-public class Habitacion {
+public class Habitacion extends ConexionBD {
     int nroHabitacion;
     int precio;
-    String tipoHabitacion;
+    int tipoHabitacion;
     Hotel hotel;
     int capacidad;
-    String descripcion;
-    
 
-public Habitacion(int nroHabitacion, int precio, String tipoHabitacion, 
-                   Hotel hotel, int capacidad, String descripcion){
-    this.nroHabitacion=nroHabitacion;
-    this.precio=precio;
-    this.tipoHabitacion=tipoHabitacion;
-    this.hotel=hotel;
-    this.capacidad=capacidad;
-    this.descripcion=descripcion;
-                 
-}
+    public Habitacion(int nroHabitacion, int precio, int tipoHabitacion, Hotel hotel, int capacidad) {
+        this.nroHabitacion = nroHabitacion;
+        this.precio = precio;
+        this.tipoHabitacion = tipoHabitacion;
+        this.hotel = hotel;
+        this.capacidad = capacidad;
+    }
+    
+    
+    public boolean grabar() {
+        boolean resp = false;
+        PreparedStatement cmd = null;   // Sentencia preparada
+        ResultSet rs;                   // Para recuperar el Id generado
+	String sql = "INSERT INTO Consorcio.personas (nroHabitacion, precio, tipoHabitacion, idHotel, capacidad) VALUES (?, ?, ?, ?, ?)";
+        
+        try {
+            //-- Se conecta a la BD
+            establecerConexion();
+            
+            //-- Carga la sentencia preparada con el script sql
+            cmd = cn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            
+            //-- Asigna parámetros a la sentencia preparada
+            cmd.setInt(1, this.nroHabitacion);
+            cmd.setInt(2, this.precio);
+            cmd.setInt(3, this.tipoHabitacion);
+            cmd.setInt(4, this.hotel.idHotel);
+            cmd.setInt(5, this.capacidad);
+
+
+            //-- Ejecuta la sentencia
+            int result = cmd.executeUpdate();
+            
+            if (result > 0) {
+                rs = cmd.getGeneratedKeys();
+                if (rs.next())
+                    throw new SQLException("No se pudo recuperar la Clave");
+                    cn.commit();
+                    resp = true;
+                }
+            else {
+                Logger.getLogger(Persona.class.getName()).log(Level.SEVERE, null, "NO SE GRABO LA CONSULTA!");
+                cn.rollback();
+            }
+        }
+        catch (SQLException e) {
+            Logger.getLogger(Persona.class.getName()).log(Level.SEVERE, null, e);
+        }
+        finally {
+
+            if (cmd != null) {
+                try {
+                    cmd.close();
+                    cmd = null;
+                }
+                catch (Exception e) {}
+            }
+            cerrarConexion();
+        }
+        
+        return resp;
+    }
+ 
+    public ArrayList<Habitacion> getHabitaciones () {
+        ArrayList<Habitacion> habitaciones = new ArrayList();
+        Statement cmd = null;
+        ResultSet rs;
+        String sql;
+
+        try {
+            establecerConexion();
+
+            cmd = cn.createStatement();
+
+            sql = "SELECT * FROM Consorcio.habitaciones";
+
+            rs = cmd.executeQuery(sql);
+
+            Hotel hotel = new Hotel();
+            
+            
+            while (rs.next()) {
+                habitaciones.add(new Habitacion(rs.getInt("nroHabitacion"),
+                                           rs.getInt("precio"),
+                                           rs.getInt("tipoHabitacion"),
+                                           hotel.getHoteles().get(rs.getInt("idHotel")),
+                                           rs.getInt("capacidad")
+                ));
+            }
+                
+            rs.close();
+            rs = null;
+        }
+        catch (SQLException e) {
+            System.err.println("Error de SQL: " + e);
+        }
+        finally {
+            if (cmd != null) {
+                try {
+                    cmd.close();
+                    cmd = null;
+                }
+                catch (SQLException e) {}
+            }
+            cerrarConexion();
+        }
+        return habitaciones;
+    }
+  
 }
 
